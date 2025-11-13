b489efb75978c94063f3c5979d45446a3b20ae6a
Marcus Nonato <marcusononato@gmail.com>
2025-11-12 22:25:56 -0400

    feat: add transactionsColumns definition with TransactionTypeBadge integration

---
+++ app/transactions/_components/transactions-columns.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { TransactionTypeBadge } from "./type-badge";
+
+export const transactionsColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original } }) => (
+      <TransactionTypeBadge transaction={original} />
+    ),
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Método de Pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+  },
+];
