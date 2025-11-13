963ebea6d4befd8de5a8c2669331de4404b43789
Marcus Nonato <marcusononato@gmail.com>
2025-11-12 23:54:59 -0400

    feat: replace button with AddTransactionButton component in TransactionsPage

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,8 +1,7 @@
-import { ArrowDownUpIcon } from "lucide-react";
-import { Button } from "../_components/ui/button";
 import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionsColumns } from "./_components/transactions-columns";
+import AddTransactionButton from "../_components/add-transaction-button";
 
 async function TransactionsPage() {
   const transactions = await db.transaction.findMany({});
@@ -11,10 +10,7 @@ async function TransactionsPage() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transaçôes</h1>
-        <Button className="rounded-full font-bold">
-          Adicionar transação
-          <ArrowDownUpIcon />
-        </Button>
+        <AddTransactionButton />
       </div>
       <DataTable columns={transactionsColumns} data={transactions} />
     </div>
