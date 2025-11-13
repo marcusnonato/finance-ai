ba654d984e088e419824443eb852a94074b4cb3c
Marcus Nonato <marcusononato@gmail.com>
2025-11-12 23:54:31 -0400

    feat: add upsertTransaction function and schema for transaction management

---
+++ app/_actions/upsert-transaction/index.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+import { upsertTransactionSchema } from "./schema";
+import { revalidatePath } from "next/cache";
+
+interface UpsertTransactionParams {
+  id?: string;
+  name: string;
+  amount: number;
+  type: TransactionType;
+  category: TransactionCategory;
+  paymentMethod: TransactionPaymentMethod;
+  date: Date;
+}
+
+export const upsertTransaction = async (params: UpsertTransactionParams) => {
+  upsertTransactionSchema.parse(params);
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  if (params.id) {
+    // Update existing transaction
+    await db.transaction.update({
+      where: {
+        id: params.id,
+      },
+      data: { ...params, userId },
+    });
+  } else {
+    // Create new transaction
+    await db.transaction.create({
+      data: { ...params, userId },
+    });
+  }
+
+  revalidatePath("/transactions");
+};
