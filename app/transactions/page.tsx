472390791af2a316ad65fad48a89839d432308cf
Marcus Nonato <marcusononato@gmail.com>
2025-11-17 18:03:40 -0400

    feat: enhance subscription and transaction pages with improved UI components and transaction management

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,35 +1,44 @@
-import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
-import { transactionsColumns } from "./_components/transactions-columns";
+import { DataTable } from "../_components/ui/data-table";
 import AddTransactionButton from "../_components/add-transaction-button";
-import { NavBar } from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+import { NavBar } from "../_components/navbar";
+import { transactionsColumns } from "./_components/transactions-columns";
 
-async function TransactionsPage() {
+const TransactionsPage = async () => {
   const { userId } = await auth();
-
   if (!userId) {
     redirect("/login");
   }
-
   const transactions = await db.transaction.findMany({
-    where: { userId },
+    where: {
+      userId,
+    },
+    orderBy: {
+      date: "desc",
+    },
   });
-
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
-    <div className="flex h-screen flex-col">
+    <>
       <NavBar />
-      <div className="flex flex-1 flex-col space-y-6 overflow-hidden p-6">
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
-          <h1 className="text-2xl font-bold">Transaçôes</h1>
-          <AddTransactionButton />
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
-        <DataTable columns={transactionsColumns} data={transactions} />
+        <ScrollArea className="h-full">
+          <DataTable
+            columns={transactionsColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
+        </ScrollArea>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default TransactionsPage;
